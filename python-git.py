7b91b36051e5fd4d0bccc3fa001461c49b3a547f
shubham malgi <ssmalgi13@gmail.com>
2026-01-27 12:05:00 +0530

    taking input from user

--- python-git.py
+++ python-git.py
@@ -1,1 +1,7 @@
-print("hello git")
+print("hello sir welcome to club")
+name = input("whats your good name sir \n")
+age = int(input("whats your age sir \n"))
+if age < 26:
+    print (f"sorry {name} sir your below 26 so you cant dring heavy drinks")
+else:
+    print (f"hi {name} sir {age} is ok  for heavy drinks")
